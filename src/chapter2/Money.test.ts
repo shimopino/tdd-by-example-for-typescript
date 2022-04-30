ba6c199a62aade1a3117566924d580bd58e1cd5f
shimopino <shimoke4869@gmail.com>
2022-05-01 01:35:24 +0900

    副作用を考慮したテストケースに変更

--- src/chapter2/Money.test.ts
+++ src/chapter2/Money.test.ts
@@ -5,12 +5,15 @@ describe('多国籍通貨の計算', () => {
   it.todo(
     '異なる通過の2つの金額を足し、通貨間の為替レートに基づいて換算された金額を得る'
   );
+
   it('金額（通貨単位あたりの額）に数値（通貨単位数）を掛け、金額を得る', () => {
     const five = new Dollar(5);
 
     five.times(2);
-
     expect(five.amount).toBe(10);
+
+    five.times(3);
+    expect(five.amount).toBe(15);
   });
 
   it.todo('amountをprivateにする');
